8656cad9863f35e97972e9837c3ffa5885302577
dhl1924 <dhl1924@naver.com>
2023-11-15 01:42:15 +0900

    refactor(Event): discount 인스턴스 변수 추가

--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -4,20 +4,30 @@ import christmas.constant.Menu;
 
 public class Event {
     private String eventName;
+    private int discount;
 
-    public Event(String eventName) {
+    public Event(String eventName, int discount) {
         this.eventName = eventName;
+        this.discount = discount;
     }
 
     public String getEventName() {
         return eventName;
     }
 
+    public int getDiscount() {
+        return discount;
+    }
+
     public static Event createChampagneEvent(int totalBeforeDiscount) {
         String eventName = "없음";
+        int discount = 0;
         if (totalBeforeDiscount >= 120000) {
             eventName = Menu.CHAMPAGNE.getName() + " 1개";
+            discount = 25000;
         }
-        return new Event(eventName);
+        return new Event(eventName, discount);
     }
 }
+
+
