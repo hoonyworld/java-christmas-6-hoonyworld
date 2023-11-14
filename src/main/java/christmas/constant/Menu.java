bcd3f3a403af9bd59d6eba8fa0186df898a4f0e4
dhl1924 <dhl1924@naver.com>
2023-11-15 01:40:59 +0900

    refactor(Menu): Category를 다시 enum으로 관리하도록 수정

--- src/main/java/christmas/constant/Menu.java
+++ src/main/java/christmas/constant/Menu.java
@@ -1,27 +1,27 @@
 package christmas.constant;
 
 public enum Menu {
-    CAESAR_SALAD("시저샐러드", 8000, "애피타이저"),
-    MUSHROOM_SOUP("양송이수프", 6000, "애피타이저"),
-    TAPAS("타파스", 5500, "애피타이저"),
+    CAESAR_SALAD("시저샐러드", 8000, Category.APPETIZER),
+    MUSHROOM_SOUP("양송이수프", 6000, Category.APPETIZER),
+    TAPAS("타파스", 5500, Category.APPETIZER),
 
-    T_BONE_STEAK("티본스테이크", 55000, "메인"),
-    BBQ_RIB("바비큐립", 54000, "메인"),
-    SEAFOOD_PASTA("해산물파스타", 35000, "메인"),
-    CHRISTMAS_PASTA("크리스마스파스타", 25000, "메인"),
+    T_BONE_STEAK("티본스테이크", 55000, Category.MAIN),
+    BBQ_RIB("바비큐립", 54000, Category.MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35000, Category.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000, Category.MAIN),
 
-    CHOCO_CAKE("초코케이크", 15000, "디저트"),
-    ICE_CREAM("아이스크림", 5000, "디저트"),
+    CHOCO_CAKE("초코케이크", 15000, Category.DESSERT),
+    ICE_CREAM("아이스크림", 5000, Category.DESSERT),
 
-    RED_WINE("레드와인", 60000, "음료"),
-    CHAMPAGNE("샴페인", 25000, "음료"),
-    ZERO_COLA("제로콜라", 3000, "음료");
+    RED_WINE("레드와인", 60000, Category.DRINK),
+    CHAMPAGNE("샴페인", 25000, Category.DRINK),
+    ZERO_COLA("제로콜라", 3000, Category.DRINK);
 
     private final String name;
     private final int price;
-    private final String category;
+    private final Category category;
 
-    Menu(String name, int price, String category) {
+    Menu(String name, int price, Category category) {
         this.name = name;
         this.price = price;
         this.category = category;
@@ -35,7 +35,14 @@ public enum Menu {
         return price;
     }
 
-    public String getCategory() {
+    public Category getCategory() {
         return category;
+    }
+
+    public enum Category {
+        APPETIZER,
+        MAIN,
+        DESSERT,
+        DRINK
     }
 }
