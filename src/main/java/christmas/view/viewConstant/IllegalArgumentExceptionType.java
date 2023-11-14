d7f99274b359b801960d9a0c57b495a2c2551abb
dhl1924 <dhl1924@naver.com>
2023-11-14 17:34:53 +0900

    refactor(IllegalArgumentExceptionType): constant에서 viewConstant로 패키지 이름 변경

--- src/main/java/christmas/view/viewConstant/IllegalArgumentExceptionType.java
+++ src/main/java/christmas/view/viewConstant/IllegalArgumentExceptionType.java
@@ -1,4 +1,4 @@
-package christmas.view.constant;
+package christmas.view.viewConstant;
 
 public enum IllegalArgumentExceptionType implements ExceptionType<IllegalArgumentException> {
     DECEMBER_DATE_OUT_OF_RANGE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
