236be4f9c7e14f4d7162d87264e4eaff94a3a528
dhl1924 <dhl1924@naver.com>
2023-11-14 17:42:34 +0900

    feat(InputView): 메뉴와 메뉴개수를 입력받아 검증하는 readMenuAndMenuCount, validateMenuInput 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,7 +1,11 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.view.constant.IllegalArgumentExceptionType;
+import christmas.constant.Menu;
+import christmas.view.viewConstant.IllegalArgumentExceptionType;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InputView {
 
@@ -18,8 +22,66 @@ public class InputView {
             }
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw IllegalArgumentExceptionType.NOT_NUMBER.getException();
+            throw IllegalArgumentExceptionType.DECEMBER_DATE_OUT_OF_RANGE.getException();
+        }
+    }
+
+    public static Map<String, Integer> readMenuAndMenuCount() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+        return validateMenuInput(input);
+    }
+
+    private static Map<String, Integer> validateMenuInput(String input) {
+        String[] menuItems = input.split(",");
+        Map<String, Integer> menuOrder = new HashMap<>();
+
+        for (String menuItem : menuItems) {
+            processMenuItem(menuItem, menuOrder);
+        }
+        return menuOrder;
+    }
+
+    private static void processMenuItem(String menuItem, Map<String, Integer> menuOrder) {
+        String[] menuDetails = menuItem.split("-");
+        String menuName = menuDetails[0];
+        validateMenuFormat(menuDetails);
+        int menuCount = parseMenuCount(menuDetails[1]);
+        validateMenuExist(menuName);
+        validateMenuCount(menuCount);
+        validateMenuDuplicate(menuName, menuOrder);
+        menuOrder.put(menuName, menuCount);
+    }
+
+    private static int parseMenuCount(String menuCountStr) {
+        try {
+            return Integer.parseInt(menuCountStr);
+        } catch (NumberFormatException e) {
+            throw IllegalArgumentExceptionType.INVALID_ORDER.getException();
+        }
+    }
+
+    private static void validateMenuDuplicate(String menuName, Map<String, Integer> menuOrder) {
+        if (menuOrder.containsKey(menuName)) {
+            throw IllegalArgumentExceptionType.INVALID_ORDER.getException();
+        }
+    }
+
+    private static void validateMenuFormat(String[] menuDetails) {
+        if (menuDetails.length != 2 || menuDetails[0].isEmpty() || menuDetails[1].isEmpty()) {
+            throw IllegalArgumentExceptionType.INVALID_ORDER.getException();
+        }
+    }
+
+    private static void validateMenuExist(String menuName) {
+        if (Arrays.stream(Menu.values()).noneMatch(menu -> menu.getName().equals(menuName))) {
+            throw IllegalArgumentExceptionType.INVALID_ORDER.getException();
+        }
+    }
+
+    private static void validateMenuCount(int menuCount) {
+        if (menuCount < 1) {
+            throw IllegalArgumentExceptionType.INVALID_ORDER.getException();
         }
     }
 }
-
