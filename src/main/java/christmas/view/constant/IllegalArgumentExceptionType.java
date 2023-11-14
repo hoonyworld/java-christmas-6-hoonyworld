640dc881bdb586ed86a0a36bd531b40afa12ee9d
dhl1924 <dhl1924@naver.com>
2023-11-14 17:12:42 +0900

    refactor(IllegalArgumentExceptionType): NOT_NUMBER 상수 삭제 및 INVALID_ORDER 상수 추가

--- src/main/java/christmas/view/constant/IllegalArgumentExceptionType.java
+++ src/main/java/christmas/view/constant/IllegalArgumentExceptionType.java
@@ -2,7 +2,7 @@ package christmas.view.constant;
 
 public enum IllegalArgumentExceptionType implements ExceptionType<IllegalArgumentException> {
     DECEMBER_DATE_OUT_OF_RANGE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    NOT_NUMBER("[ERROR] : 숫자만 입력이 가능합니다");
+    INVALID_ORDER("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     private final String message;
 
     IllegalArgumentExceptionType(String message) {
