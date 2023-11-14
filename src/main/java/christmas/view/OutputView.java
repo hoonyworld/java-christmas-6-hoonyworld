db0a695201f08ef337a124648150566b11547376
dhl1924 <dhl1924@naver.com>
2023-11-15 01:47:45 +0900

    feat(OutputView): 혜택 내역, 총혜택 금액, 할인 후 예상 결제 금액, 이벤트 배지 출력하는 메서드 생성

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,6 +3,8 @@ package christmas.view;
 import christmas.constant.Menu;
 import christmas.domain.Event;
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class OutputView {
@@ -41,4 +43,51 @@ public class OutputView {
         System.out.println(event.getEventName());
         System.out.println();
     }
+
+    public static void printBenefitsDetails(int christmasDayDiscount, int weekdayDiscount, int weekendDiscount,
+                                            int specialDiscount, Event event) {
+        System.out.println("<혜택 내역>");
+        List<String> benefits = new ArrayList<>();
+        if (christmasDayDiscount != 0) {
+            benefits.add("크리스마스 디데이 할인: " + "-" + formatPrice(christmasDayDiscount) + "원");
+        }
+        if (weekdayDiscount != 0) {
+            benefits.add("평일 할인: " + "-" + formatPrice(weekdayDiscount) + "원");
+        }
+        if (weekendDiscount != 0) {
+            benefits.add("주말 할인: " + "-" + formatPrice(weekendDiscount) + "원");
+        }
+        if (specialDiscount != 0) {
+            benefits.add("특별 할인: " + "-" + formatPrice(specialDiscount) + "원");
+        }
+        if (event.getDiscount() != 0) {
+            benefits.add("증정 이벤트: " + "-" + formatPrice(event.getDiscount()) + "원");
+        }
+        if (benefits.isEmpty()) {
+            System.out.println("없음");
+        }
+        for (String benefit : benefits) {
+            System.out.println(benefit);
+        }
+        System.out.println();
+    }
+
+    public static void printTotalBenefits(int totalBenefits) {
+        System.out.println("<총혜택 금액>");
+        System.out.println("-" + formatPrice(totalBenefits) + "원");
+        System.out.println();
+    }
+
+    public static void printTotalPriceAfterDiscount(int totalPriceAfterDiscount) {
+        System.out.println("<할인 후 예상 결제 금액>");
+        System.out.println(formatPrice(totalPriceAfterDiscount) + "원");
+        System.out.println();
+    }
+
+    public static void printBadge(String badge) {
+        System.out.println("<12월 이벤트 배지>");
+        System.out.println(badge);
+        System.out.println();
+    }
+
 }
