dec3a618975e063926b867bdd31979c16d426236
dhl1924 <dhl1924@naver.com>
2023-11-14 22:24:11 +0900

    feat(OutputView): 할인 전 총주문 금액, 증정 메뉴를 출력하는 메서드 생성

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,8 @@
 package christmas.view;
 
+import christmas.constant.Menu;
+import christmas.domain.Event;
+import java.text.NumberFormat;
 import java.util.Map;
 
 public class OutputView {
@@ -12,12 +15,30 @@ public class OutputView {
         System.out.println();
     }
 
-    public static void printMenu(Map<String, Integer> menuOrder) {
+    public static void printMenu(Map<Menu, Integer> menuOrder) {
         System.out.println("<주문 메뉴>");
-        for (Map.Entry<String, Integer> entry : menuOrder.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue() + "개");
+        for (Map.Entry<Menu, Integer> entry : menuOrder.entrySet()) {
+            System.out.println(entry.getKey().getName() + " " + entry.getValue() + "개");
         }
         System.out.println();
     }
 
+    public static void printTotalPriceBeforeDiscount(int total) {
+        System.out.println("<할인 전 총주문 금액>");
+        System.out.println(formatPrice(total) + "원");
+        System.out.println();
+    }
+
+
+    private static String formatPrice(int price) {
+        NumberFormat formatter = NumberFormat.getInstance();
+        return formatter.format(price);
+    }
+
+    public static void printBenefits(int totalBeforeDiscount) {
+        System.out.println("<증정 메뉴>");
+        Event event = Event.createChampagneEvent(totalBeforeDiscount);
+        System.out.println(event.getEventName());
+        System.out.println();
+    }
 }
