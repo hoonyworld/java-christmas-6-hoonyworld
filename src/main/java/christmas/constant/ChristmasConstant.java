b3a33473d7ae98e9ff90935b90a3b822e3c4970f
dhl1924 <dhl1924@naver.com>
2023-11-15 17:58:19 +0900

    feat(ChristmasConstant): domain에 사용되는 크리스마스 상수 클래스 생성

---
+++ src/main/java/christmas/constant/ChristmasConstant.java
@@ -0,0 +1,27 @@
+package christmas.constant;
+
+public class ChristmasConstant {
+    public static final int MIN_TOTAL_PRICE_FOR_EVENT = 10000;
+    public static final int CHRISTMAS_DAY_BASE_DISCOUNT = 1000;
+    public static final int WEEKDAY_DESSERT_DISCOUNT = 2023;
+    public static final int WEEKEND_MAIN_DISCOUNT = 2023;
+    public static final int SPECIAL_DISCOUNT = 1000;
+    public static final int CHRISTMAS_DAY_START = 1;
+    public static final int CHRISTMAS_DAY_END = 25;
+    public static final int HUNDRED_WON = 100;
+    public static final int GIVE_CHAMPAGNE_THRESHOLD = 120000;
+    public static final int CHAMPAGNE_DISCOUNT_AMOUNT = 25000;
+    public static final int FIRST_THRESHOLD = 20000;
+    public static final int SECOND_THRESHOLD = 10000;
+    public static final int THIRD_THRESHOLD = 5000;
+
+
+    public static final String NO_CHAMPAGNE = "없음";
+    public static final String ONE_CHAMPAGNE_SERVE = "1개";
+    public static final String STAR_BADGE = "별";
+    public static final String TREE_BADGE = "트리";
+    public static final String SANTA_BADGE = "산타";
+
+    private ChristmasConstant() {
+    }
+}
