64a05ca465b6ad87521c7b084bd6f3a2d1fb99f9
dhl1924 <dhl1924@naver.com>
2023-11-15 13:41:42 +0900

    feat(MessageConstant): 메시지 상수를 관리하는 클래스 생성

---
+++ src/main/java/christmas/view/viewConstant/MessageConstant.java
@@ -0,0 +1,25 @@
+package christmas.view.viewConstant;
+
+public class MessageConstant {
+    public static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String DATE_PROMPT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String MENU_ORDER_PROMPT_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String DATE_MESSAGE_PREFIX = "12월 ";
+    public static final String DATE_MESSAGE_SUFFIX = "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String ORDER_MENU_HEADER = "<주문 메뉴>";
+    public static final String ORDER_MENU_COUNT = "개";
+    public static final String TOTAL_PRICE_BEFORE_DISCOUNT_HEADER = "<할인 전 총주문 금액>";
+    public static final String WON_SUFFIX = "원";
+    public static final String BENEFITS_MENU_HEADER = "<증정 메뉴>";
+    public static final String BENEFITS_DETAILS_HEADER = "<혜택 내역>";
+    public static final String CHRISTMAS_DAY_DISCOUNT = "크리스마스 디데이 할인";
+    public static final String WEEKDAY_DISCOUNT = "평일 할인";
+    public static final String WEEKEND_DISCOUNT = "주말 할인";
+    public static final String SPECIAL_DISCOUNT = "특별 할인";
+    public static final String EVENT_DISCOUNT = "증정 이벤트";
+    public static final String MINUS_PREFIX = "-";
+    public static final String NO_BENEFITS_MESSAGE = "없음";
+    public static final String TOTAL_BENEFITS_HEADER = "<총혜택 금액>";
+    public static final String TOTAL_PRICE_AFTER_DISCOUNT_HEADER = "<할인 후 예상 결제 금액>";
+    public static final String EVENT_BADGE_HEADER = "<12월 이벤트 배지>";
+}
