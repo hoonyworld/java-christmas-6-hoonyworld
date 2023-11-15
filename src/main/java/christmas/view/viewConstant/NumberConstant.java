ceb51a634f0f44e1dda46623ec5df3a52c2991ad
dhl1924 <dhl1924@naver.com>
2023-11-15 13:42:05 +0900

    feat(NumberConstant): 숫자 상수를 관리하는 클래스 생성

---
+++ src/main/java/christmas/view/viewConstant/NumberConstant.java
@@ -0,0 +1,8 @@
+package christmas.view.viewConstant;
+
+public class NumberConstant {
+    public static final int MIN_DATE = 1;
+    public static final int MAX_DATE = 31;
+    public static final int MIN_MENU_COUNT = 1;
+    public static final int MENU_DETAILS_LENGTH = 2;
+}
